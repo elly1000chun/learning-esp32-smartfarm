02bb87a612ee5aeaba949dfee1bf986408af0354
EllyChun <chun1000mk@gmail.com>
2026-04-23 15:06:32 +0900

    Imporoved variable naming

---
+++ 01.BlinkLED/ReadMe.md
@@ -0,0 +1,3 @@
+# Blink LED
+
+- PlatformIO를 활용한 프로젝트 셋업 및 보드 동작 확인. 
--- 01.BlinkLED/src/main.cpp
+++ 01.BlinkLED/src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 
 #define LED 2
-#define GPIO 13
+#define GPIO_LED 13
 
 void setup() {
   Serial.begin(115200);
-  pinMode(GPIO, GPIO_MODE_OUTPUT);
 }
 
 void loop() {
-  digitalWrite(GPIO, HIGH);
+  digitalWrite(GPIO_LED, HIGH);
   Serial.println("LED is on");
   delay(1000);
-  digitalWrite(GPIO, LOW);
+  digitalWrite(GPIO_LED, LOW);
   Serial.println("LED is off");
   delay(1000);
 }
