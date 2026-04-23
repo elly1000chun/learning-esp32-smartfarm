c22012e4d593d507c8388859edbf40ff9c4fe89a
EllyChun <chun1000mk@gmail.com>
2026-04-23 12:48:43 +0900

    Blink LED test

--- 01.BlinkLED/src/main.cpp
+++ 01.BlinkLED/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED 2
+#define GPIO 13
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(GPIO, GPIO_MODE_OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(GPIO, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(GPIO, LOW);
+  Serial.println("LED is off");
+  delay(1000);
 }
