bae03b4f899a5b16639dd735fb4d43a0714242c4
EllyChun <chun1000mk@gmail.com>
2026-04-24 13:18:35 +0900

    print sensor data to oled

--- 04.oled-display/src/main.cpp
+++ 04.oled-display/src/main.cpp
@@ -1,18 +1,62 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true) {}
+  }
+
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  display.println(F("Wi-Fi Connecting"));
+  display.display();
+
+  for(int i = 0; i < 10; i++)
+  {
+    display.print(F("."));
+    delay(200);
+    display.display();
+  }
+
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print(F("Wi-Fi Connected."));
+  display.display();
+
+  delay(1000);
+  
+}
+
+int rand5()
+{
+  double R = (double)rand() / (double)RAND_MAX;
+  int val = (int)((R*10)-5 + 0.5);
+  return val;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int temperature = 23+ rand5();
+  String tem = "Tem: " + String(temperature) + (char)247 + "C";
+  int humidity = 40+ rand5();
+  String hum = "Hum: " + String(humidity) + "%";
+  int dryness = 60+ rand5();
+  String dry = "Dry: " + String(dryness) + "%";
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println(tem);
+  display.println(hum);
+  display.println(dry);
+  display.display();
+
+  delay(1000);
 }
