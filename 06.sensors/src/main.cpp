02584fa32f00da924676833670ca67cbc0b48b05
EllyChun <chun1000mk@gmail.com>
2026-04-24 16:51:15 +0900

    sensing temperature, humidity, soil dryness

--- 06.sensors/src/main.cpp
+++ 06.sensors/src/main.cpp
@@ -1,16 +1,82 @@
 #include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
+#define DHTPIN 27    // Digital pin connected to the DHT sensor 
+#define DHTTYPE DHT22     // DHT 22 (AM2302)
+
+#define SOILPIN 26
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  // Initialize device.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+
+  Serial.print("Delay ms: ");
+  Serial.println(delayMS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+
+  int potValue = analogRead(SOILPIN);  
+  int dryness = (int)(((double) (potValue - 1000) / (double)4095.0 * 100.0) + 0.5);
+  Serial.print(F("Analog 0: "));
+  Serial.println(potValue);
+  Serial.print(F("Soil Dryness: "));
+  Serial.print(dryness);
+  Serial.println("%");
+
+  // Delay between measurements.
+  delay(delayMS);
 }
