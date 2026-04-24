2fb670bc7d899d32697969744df522ac0bb5e0e1
EllyChun <chun1000mk@gmail.com>
2026-04-24 12:11:25 +0900

    Deep sleep test

--- 01.BlinkLED/src/main.cpp
+++ 01.BlinkLED/src/main.cpp
@@ -3,16 +3,47 @@
 #define LED 2
 #define GPIO_LED 13
 
+#define uS_TO_S_FACTOR 1000000 
+#define TIME_TO_SLEEP  5        //sec
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(GPIO_LED, GPIO_MODE_OUTPUT);
+  print_wakeup_reason();
 }
 
-void loop() {
-  digitalWrite(GPIO_LED, HIGH);
-  Serial.println("LED is on");
+void loop()
+{
+  for(int i = 0; i < 10; i++)
+  {
+    digitalWrite(GPIO_LED, HIGH);
+    Serial.println("LED is on");
+    delay(500);
+    digitalWrite(GPIO_LED, LOW);
+    Serial.println("LED is off");
+    delay(500);
+  }
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+
+  Serial.println("Going to sleep now");
   delay(1000);
-  digitalWrite(GPIO_LED, LOW);
-  Serial.println("LED is off");
-  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
 }
