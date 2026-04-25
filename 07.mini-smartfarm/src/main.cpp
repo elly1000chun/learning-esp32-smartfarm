c2dc15f7398dfdf4f78d62b6ff91ec02fd85a404
EllyChun <chun1000mk@gmail.com>
2026-04-25 10:10:37 +0900

    added OLED Controller.

--- 07.mini-smartfarm/src/main.cpp
+++ 07.mini-smartfarm/src/main.cpp
@@ -2,15 +2,39 @@
 #include <WiFi.h>
 #include "network/thingspeak.hpp"
 #include "network/wifiController.hpp"
+#include "display/OledController.h"
+
+OledController oled;
+
+bool isDisplayAvailable = false;
+bool isWifiConnected = false;
 
 void setup() {
   Serial.begin(115200);
+
+  // Init display
+  isDisplayAvailable = oled.Initialize();
+  if(isDisplayAvailable == false)
+  {
+    // don't do anything
+    while(1);
+  }
+
+  oled.SetTextSize(1);
+  oled.PrintLine("Welcome.");
+  delay(1000);
+  oled.PrintLine("Wi-Fi Connecting...");
  
   int counter = 0;
-  bool isConnected = network::ConnectWifi();
+  isWifiConnected = network::ConnectWifi();
 
-  if(isConnected == false)
+  if(isWifiConnected == true)
   {
+    oled.PrintLine("Wi-Fi Connected.");
+  }
+  else
+  {
+    oled.PrintLine("Wi-Fi Error.");
     // don't do anything
     while(1);
   }
