3b48e14652ead6534b65f529209e76bc7a5ae73c
EllyChun <chun1000mk@gmail.com>
2026-04-25 09:33:34 +0900

    added network codes

--- 07.mini-smartfarm/src/main.cpp
+++ 07.mini-smartfarm/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
+#include "network/thingspeak.hpp"
+#include "network/wifiController.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+ 
+  int counter = 0;
+  bool isConnected = network::ConnectWifi();
+
+  if(isConnected == false)
+  {
+    // don't do anything
+    while(1);
+  }
+}
+
+int count = 0;
+
+int rand5()
+{
+  double R = (double)rand() / (double)RAND_MAX;
+  int val = (int)((R*10)-5 + 0.5);
+  return val;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(count > 2) 
+  {
+    delay(1000);
+    return;
+  }
+  
+  // send dummy value for now
+  network::SendDataThingspeak(23 + rand5(), 40+ rand5(), 60+ rand5());
+  delay(10000);
+  count++;
 }
